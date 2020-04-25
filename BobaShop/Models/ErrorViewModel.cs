7f6999cc6e20cf4f615d24c70631069161254ebd
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-04-25 19:34:08 -0400

    Add project files.

---
+++ BobaShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BobaShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
